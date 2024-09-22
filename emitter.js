bd2bb34784a4c20cd784c8d73bfc991480e88ea4
NastyaParnet <nastya.parnet.01@gmail.com>
2024-09-22 12:48:59 +0300

    ready task

--- emitter.js
+++ emitter.js
@@ -1,1 +1,20 @@
-module.exports = // Your code here
+const { interval, map, take, filter } = require("rxjs");
+
+const observable = interval(500).pipe(
+  map((value) => {
+    if(value === 10) {
+      throw new Error("Explosion!");
+    }
+    return value;
+  }),
+  take(14),
+  filter(value => typeof value !== 'number' || value % 2 === 1),
+  map((value) => {
+    if(typeof value === 'number') {
+      return 'tick';
+    }
+    return value;
+  }),
+)
+
+module.exports = observable
--- main.js
+++ main.js
@@ -1,6 +1,22 @@
+const { of, map, catchError } = require("rxjs");
 const clock$ = require("./emitter");
 
 const subscription = clock$
-  // Your code here
+  .pipe(
+    map((value, index) => {
+      if(index%2 === 1) return 'tock';
+      return value;
+    }),
+    catchError((err) => {
+      console.log(err.message);
+      return of();
+    })
+  )
 
-module.exports = subscription;
+const observer = {
+  next: (x) => console.log(x),
+  error: (err) => console.log(err),
+  complete: () => console.log('Observer got a complete notification'),
+}
+
+module.exports = subscription.subscribe(observer);
